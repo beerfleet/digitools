496b024924cb802c53af03a8b9a7bfab8c3e876c
beerfleet <jvbiervliet@gmail.com>
2016-02-28 15:05:48 +0100

    + custom valitron rule to check for existing client

--- src/digi/EslTools/Service/Validation/EslClientValidation.php
+++ src/digi/EslTools/Service/Validation/EslClientValidation.php
@@ -3,13 +3,28 @@
 namespace digi\eslTools\Service\Validation;
 
 use Valitron\Validator;
+use Slim\Slim;
 
 class EslClientValidation extends Validation {
   
+  public function __construct($app, $em) {
+    /* @var $app Slim */
+    Validator::addRule('unique_storename', function($field, $value, array $params) use($em, $app) {
+      $storename = $app->request->post('winkelnaam');
+      $repo = $em->getRepository('digi\eslTools\Entities\Store');
+      $result = $repo->findBy(array('storename' => $storename));
+      return count($result) < 1;
+    }, 'bestaat al');
+    
+    parent::__construct($app, $em);
+  }
+  
   public function addRules() {
     $val = $this->getVal();
-    $val->rule('email', 'e-mail')->message('{field} is geen geldig e-mail adres');
+    $email = $this->getApp()->request->post('e-mail');
+    $val->rule('email', 'e-mail')->message("$email is geen geldig e-mail adres");
     $val->rule('numeric', 'synergie');
-    
+    $winkel = $this->getApp()->request->post('winkelnaam');
+    $val->rule('unique_storename', 'winkelnaam')->message("$winkel bestaat al");
   }
 }

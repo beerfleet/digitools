c8ec0cd3e543ff296d7d9464fc06addc41696653
Jan@Digi <jvbiervliet@gmail.com>
2016-03-07 16:41:23 +0100

    + TODO - new - set checked state with jquery - start

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+$(function() {
+  initFinishstate();
+});
 
+function initFinishstate() {
+  $('.finishstate').on('click', function() {    
+    id = $(this).data('todo-id');
+    alert(id);
+  });
+}
 
-
+function test() {
+  console.out('test');
+}

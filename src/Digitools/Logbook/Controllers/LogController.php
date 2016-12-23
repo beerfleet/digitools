763df1ebaca50cf217e50e4481795a8ad569d16e
beerfleet <jvbiervliet@gmail.com>
2016-12-23 12:59:58 +0100

    log controller show_form

--- src/Digitools/Logbook/Controllers/LogController.php
+++ src/Digitools/Logbook/Controllers/LogController.php
@@ -6,24 +6,30 @@ use Doctrine\ORM\EntityManager;
 use Slim\Slim;
 use Digitools\Common\Controllers\Controller;
 use Digitools\Logbook\Service\LogService;
+use Digitools\Logbook\Entities\Log;
+use Exception;
 
 /**
  * @author jan
  */
 class LogController extends Controller {
+  /* @var $srv LogService */
 
-  /* @var $srv LogbookSrv */
   private $srv;
   private $em;
   private $app;
+  /* @var $log Log */
+  private $log;
 
   public function __construct($em, $app) {
     parent::__construct($em, $app);
-    $this->srv = new EslService($em, $app);
+    $this->srv = new LogService($em, $app, $this->getUser());
+    $this->em = $em;
   }
-  
-  public function testMakeLog() {
-    echo("TEST MAKE LOG");
+
+  public function show_log_form() {
+    $this->srv->insert_log(new Log());    
+    
   }
 
 }

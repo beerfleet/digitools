26e1e5452ff86a68ff90ebb6fd84bec691ebdaff
beerfleet <jvbiervliet@gmail.com>
2016-12-23 12:54:10 +0100

    start log service layer

--- src/Digitools/Logbook/Service/LogService.php
+++ src/Digitools/Logbook/Service/LogService.php
@@ -12,4 +12,31 @@ namespace Digitools\Logbook\Service;
  */
 class LogService {
   
+  private $errors;
+  private $user;
+  private $em;
+  private $app;
+  
+  
+  function __construct($em, $app, $user) {
+    $this->errors = null;
+    $this->em = $em;
+    $this->app = $app;
+    $this->user = $user;
+  }
+
+  public function insert_log($log) {
+    $log->set_user($this->user);
+    $log->set_entry("This is starlog Alfa Draco Tango");
+    $log->set_created(new \DateTime());
+    $em = $this->em;
+    try {
+      $em->persist($log);
+      $em->flush();
+      echo("Persisted OK");
+    } catch (Exception $e) {
+      echo($e->getMessage());
+    }
+  }
+
 }

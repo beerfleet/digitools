e343057805937117e38aea360b9d5d2cbb6619cf
beerfleet <jvbiervliet@gmail.com>
2016-12-27 15:22:29 +0100

    searchable log listbox

--- js/list.js
+++ js/list.js
@@ -1,22 +1,44 @@
-$(document).ready (function () {
-  console.log("AHAHAHA");
+$(document).ready(function () {
+  init_search();
+  init_search_handling();
+  console.log("yayayayay");
 });
 
-function search_list() {
-  // Declare variables
-  var input, filter, ul, li, a, i;
-  input = document.getElementById('myInput');
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("myUL");
-  li = ul.getElementsByTagName('li');
+function init_search() {
+  $('.search_list li').each(function () {
+    $(this).attr('data-search-term', $(this).text().toLowerCase());
+  });
+}
 
-  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
-  }
+function init_search_handling() {
+  $('.search_box').on('keyup', function () {
+    var searchTerm = $(this).val().toLowerCase();
+    $('.search_list li').each(function () {
+      if ($(this).filter('[data-search-term *= ' + searchTerm + ']').length > 0 || searchTerm.length < 1) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+    });
+  });
+}
+
+function search_list() {
+
+  // Declare variables
+  /*var input, filter, ul, li, a, i;
+   input = document.getElementById('myInput');
+   filter = input.value.toUpperCase();
+   ul = document.getElementById("myUL");
+   li = ul.getElementsByTagName('li');
+   
+   // Loop through all list items, and hide those who don't match the search query
+   for (i = 0; i < li.length; i++) {
+   a = li[i].getElementsByTagName("a")[0];
+   if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+   li[i].style.display = "";
+   } else {
+   li[i].style.display = "none";
+   }
+   }*/
 }

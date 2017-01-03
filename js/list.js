00fd8a39afadcd33291ba2feee8d2c4bd3e4f989
beerfleet <jvbiervliet@gmail.com>
2017-01-03 13:43:47 +0100

    bugfix: filtering works now, all fields can be searchable

--- js/list.js
+++ js/list.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
 // list
 function init_search() {
   $('.search_list li').each(function () {
-    srch_terms = $(this).find(".item_created").text() + " " + $(this).find(".item_entry_box").text();
+    srch_terms = $(this).find(".item_created").text().toLowerCase() + " " + $(this).find(".item_entry_box").text().toLowerCase();
     console.log(srch_terms);
     $(this).attr('data-search-term', srch_terms);
   });
@@ -17,6 +17,7 @@ function init_search_handling() {
   $('.search_box').on('keyup', function () {
     var searchTerm = $(this).val().toLowerCase();    
     $('.search_list li').each(function () {
+      //if ($(this).filter('[data-search-term *= ' + searchTerm + ']').length > 0 || searchTerm.length < 1) {
       if ($(this).filter('[data-search-term *= ' + searchTerm + ']').length > 0 || searchTerm.length < 1) {
         $(this).show();
       } else {

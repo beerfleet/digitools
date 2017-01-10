ad9636ebc2eeea26d917544cc24152c99b224a5f
beerfleet <jvbiervliet@gmail.com>
2017-01-10 17:30:09 +0100

    WTF MET NEW_TAG

--- js/tags.js
+++ js/tags.js
@@ -4,7 +4,7 @@
 
 $(document).ready(function () {
   init_onchange_event();
-  new_tag();
+  //new_tag();
   init_verwijder_tags();
 });
 
@@ -39,7 +39,7 @@ function init_onchange_event() {
 
 // tags
 function new_tag() {
-  $('.tag-collection .nieuwe-tag a').on('click', function () {
+  $('.tag-collection .nieuwe-tag a').on('click', function () {    
     $(this).hide();
     add_input_box();
     tag_input_not_empty();

a4752cf55c3f4f9cbd9a8dc593d20a872221e117
beerfleet <jvbiervliet@gmail.com>
2017-01-10 22:40:49 +0100

    start todo rework

--- js/todo.js
+++ js/todo.js
@@ -1,13 +1,22 @@
 $(function () {
   initPickers();
+  init_datatable();
 });
 
 function initPickers() {
+  if ( $(".datepick").size() <= 0 ) {
+    return;
+  }
   $(".datepick").datepicker();
   $(".timepick").timepicker();
   $(".datepick, .timepick").on('mouseover', function() {
   	$('.datepick, .timepick').css( 'cursor', 'pointer' );
   });
+}
+
+function init_datatable() {
+  if ($(".dataTable").size() <= 0) return;
+  $('.dataTable').DataTable();  
 }
 
 /*
